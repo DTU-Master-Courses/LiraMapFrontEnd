22bea47ebb49d396b983f77b679a7639a6491eb9
CookieNess <frederikkjaergaard@yahoo.dk>
2022-09-29 23:18:19 +0200

    Created dummy draggable/resizable box

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import MapDemo from "./Components/Map/Map";
 import { FC } from "react";
 import * as L from 'leaflet';
+import DraggableComponent from "./Components/Drawer/DraggableComponent";
 
 const App: FC = () => {
   const position = L.marker([55.7856,12.5214]);
   return (
     <div className="App">
       <MapDemo position={position.getLatLng()} />
+      <DraggableComponent></DraggableComponent>
     </div>
   );
 }
