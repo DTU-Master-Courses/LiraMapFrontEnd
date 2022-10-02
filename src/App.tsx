4ec258a588244b1f0774377a57f870de03064c84
Alexander Johansson <alexander@shape.dk>
2022-10-02 16:40:32 +0200

    Fixes correct removal of draggable windows

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,14 @@ import * as L from 'leaflet';
 import NavBar from "./Components/NavBar/NavBar";
 import DraggableComponent from "./Components/Drawer/DraggableComponent";
 
-
 const App: FC = () => {
   const [componentsList, setComponentsList] = useState([{component: ''}]);
   const position = L.marker([55.7856,12.5214]);
+  const [uniqueId, setUniqueId] = useState(0);
 
   const addComponent = () => {
-    setComponentsList([...componentsList, {component: ''}]);
+    setUniqueId(uniqueId + 1)
+    setComponentsList([...componentsList, {component: `Draggable${uniqueId}`}]);
   }
 
   const removeComponent = (index:number) => {
@@ -26,7 +27,7 @@ const App: FC = () => {
       <NavBar addComponent={addComponent}></NavBar>
       <MapDemo position={position.getLatLng()} />
       {componentsList.map((component, index) => (
-        <DraggableComponent removeComponent={removeComponent} index={index}></DraggableComponent>
+        <DraggableComponent key={component.component} removeComponent={removeComponent} index={index}></DraggableComponent>
       ))}
     </div>
   );
