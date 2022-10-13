bbef857522a1f09c17e077f6123264c81553af57
CookieNess <frederikkjaergaard@yahoo.dk>
2022-10-13 19:04:42 +0200

    Implemented the API to display on the map
    Co-Authored-By: Gustav Onsberg <17405150+GustavOnsberg@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App: FC = () => {
   	const [uniqueId, setUniqueId] = useState(0);
   	const [uniqueZ, setUniqueZ] = useState(0);
 
-  	const focusWindow = (windowId: number) => {
+  	const focusWindow = (windowId : number) => {
 		if (windowInFocus !== windowId) {
 			setWindowInFocus(windowId);
 			setUniqueZ(uniqueZ + 100);
@@ -33,30 +33,16 @@ const App: FC = () => {
 		setGraphComponentsList([...graphComponentsList, {componentId: uniqueId, graphTaskID: taskID, graphTripID: tripID}]);
 		let points;
 		try {
-            points = await fetch(`http://localhost:8000/trips/acceleration/${tripID}`).then((response) => response.json());
-			console.log(points);
+            points = await fetch(`http://localhost:8000/trips/segments/${tripID}`).then((response) => response.json());
         } catch (err) {
             console.log(err);   
-        }
+        }	
+
+    	let newPolyPoints:[number, number][] = [];
 		for (let i = 0; i < points.length; i++) {
-			
+      		newPolyPoints.push([points[i]["lat"],points[i]["lon"]])
 		}
-		setPolyLinePoints([...polyLinePoints, [[55.68173074538492, 12.558783990421695],
-      [55.68099477243995, 12.560828719024013],
-      [55.68023924596522, 12.562792582603771],
-      [55.67923619691593, 12.564282808534271],
-      [55.678656500996745, 12.564987489013037],
-      [55.67907987526686, 12.565900107993734],
-      [55.68029135170652, 12.567390333924234],
-      [55.68105339024206, 12.568476234989872],
-      [55.68219967665559, 12.569746970279446],
-      [55.683775765668706, 12.572103606514087],
-      [55.68461940014825, 12.57353542800247],
-      [55.68529258278767, 12.57097302286676],
-      [55.6857273404169, 12.569181827043735],
-      [55.68495599291647, 12.568286229132223],
-      [55.68533465631786, 12.56726624262189],
-      [55.68613404479112, 12.565325780480283]]]); //TODO: Do the stuff here
+		setPolyLinePoints([...polyLinePoints, newPolyPoints]);
 	}
 
 	const removeGraphComponent = (index: number) => {
