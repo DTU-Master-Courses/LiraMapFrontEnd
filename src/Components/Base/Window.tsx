cd81ed6676613a2eae639c89544ace36679b45ef
CookieNess <frederikkjaergaard@yahoo.dk>
2022-11-24 13:48:52 +0100

    Added button for day-night filtering + csv download button

--- src/Components/Base/Window.tsx
+++ src/Components/Base/Window.tsx
@@ -1,6 +1,7 @@
 import { Rnd } from "react-rnd";
 import React, { useState } from "react";
 import "../Drawer/DrawerComponents.css";
+import { Button } from "@mui/material";
 
 interface WindowProps {
   id: number;
@@ -65,6 +66,10 @@ const Window = ({
           className="close_component_btn"
           onClick={() => closeWindow(id)}
         ></button>
+        <Button sx={{marginLeft: -1.8}}
+          style={{ display: hidable ? "inline-block" : "none" }}
+          className="download_csv_button"
+        ></Button>
       </div>
       <div className="draggable_component_container_content">
         {React.Children.only(children)}
