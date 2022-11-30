a779bfdd130945c5f4fc91d752f3c1db4c53203f
PossibleNPC <28771786+PossibleNPC@users.noreply.github.com>
2022-11-30 11:55:18 +0100

    physics json download integration

--- src/Components/Base/Window.tsx
+++ src/Components/Base/Window.tsx
@@ -2,6 +2,8 @@ import { Rnd } from "react-rnd";
 import React, { useState } from "react";
 import "../Drawer/DrawerComponents.css";
 import { Button, Tooltip } from "@mui/material";
+import Hostname from "../Utils/hostname";
+import ClientRequestHeaders from "../Utils/client-request-headers";
 
 interface WindowProps {
   id: number;
@@ -36,6 +38,21 @@ const Window = ({
 
   const example = windowName?.split(": ")[1];
   console.log(example);
+
+  const downloadCsv = async () => {
+    const tripDetailsResponse = await fetch(
+        `http://${Hostname}:8000/trips/physics/id/${example}`,
+        { headers: ClientRequestHeaders }
+    );
+    const physicsJson = await tripDetailsResponse.json();
+
+    const a = document.createElement("a");
+    const file = new Blob([JSON.stringify(physicsJson, null, 2)], {type: "text/json"});
+    a.href = URL.createObjectURL(file);
+    a.download = `trip-${example}.json`;
+    a.click();
+    a.remove();
+  };
 
   return (
     <Rnd
@@ -74,6 +91,7 @@ const Window = ({
             sx={{ marginLeft: -1.8, height: 17, marginTop: 0.2 }}
             style={{ display: hidable ? "inline-block" : "none" }}
             className="download_csv_button"
+            onClick={downloadCsv}
           ></Button>
         </Tooltip>
       </div>
